ff076b8175f5ac38406bbbcd30e54b23ffd04396
Dang Minh <minhdangngoc304@gmail.com>
2025-04-08 15:24:37 +0700

    handle exception, response data

---
+++ src/main/java/com/danmin/home_service/exception/InvalidDataException.java
@@ -0,0 +1,11 @@
+package com.danmin.home_service.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+
+    public InvalidDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
