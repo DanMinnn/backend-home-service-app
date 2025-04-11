63372822682c0542afbe857c2b8bf1cf3901ff85
Dang Minh <minhdangngoc304@gmail.com>
2025-04-11 09:33:24 +0700

    add config

--- src/main/java/com/danmin/home_service/config/AppConfig.java
+++ src/main/java/com/danmin/home_service/config/AppConfig.java
@@ -4,10 +4,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.sendgrid.SendGrid;
 
@@ -20,18 +24,51 @@ public class AppConfig {
     @Value("${spring.sendgrid.api-key}")
     private String sendGridApiKey;
 
+    // config for security filter chain
+    // This is used to configure the security filter chain for the application
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.csrfTokenRepository(new CookieCsrfTokenRepository())
-                .requireCsrfProtectionMatcher(httpServletRequest -> httpServletRequest.getMethod().equals("none")));
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        request -> request.requestMatchers("/**").permitAll().anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
+    // config for web security customizer
+    // This is used to customize the web security for the application
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (webSecurity) -> webSecurity
+                .ignoring()
+                .requestMatchers("/api-docs/**", "/swagger-ui/**");
+    }
+
+    // config for CORS
+    // This is used to configure CORS for the application
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
+                        .allowedHeaders("*") // Allowed request headers
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+
+    // config for password encoder
+    // This is used to encode the password before saving it to the database
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    // config for sendgrid
     @Bean
     public SendGrid sendGrid() {
         return new SendGrid(sendGridApiKey);
